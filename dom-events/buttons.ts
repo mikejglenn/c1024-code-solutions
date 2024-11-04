2f6c1b4288ec5583d1e2d42199cbba7845977e79
Mike Glenn <mjglenn+lf@gmail.com>
2024-11-04 19:38:51 +0000

    Completed dom-events exercise.

---
+++ dom-events/buttons.ts
@@ -0,0 +1,29 @@
+const $button = document.querySelector('.click-button');
+if (!$button) throw new Error('$button does not exist');
+
+function handleClick(event: Event): void {
+  console.log('button clicked');
+  console.log('value of event', event);
+  console.log('value of event.target', event.target);
+}
+$button.addEventListener('click', handleClick);
+
+const $hoverButton = document.querySelector('.hover-button');
+if (!$hoverButton) throw new Error('$hoverButton does not exist');
+
+function handleMouseover(event: Event): void {
+  console.log('button hovered');
+  console.log('value of event', event);
+  console.log('value of event.target', event.target);
+}
+$hoverButton.addEventListener('mouseover', handleMouseover);
+
+const $doubleButton = document.querySelector('.double-click-button');
+if (!$doubleButton) throw new Error('$doubleButton does not exist');
+
+function handleDoubleClick(event: Event): void {
+  console.log('button double-clicked');
+  console.log('value of event', event);
+  console.log('value of event.target', event.target);
+}
+$doubleButton.addEventListener('dblclick', handleDoubleClick);
