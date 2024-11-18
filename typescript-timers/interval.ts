3307ed8373596f4ea0b40d7355d8a6e940316bef
Mike Glenn <mjglenn+lf@gmail.com>
2024-11-18 19:26:44 +0000

    fixed timer count to 1.

--- typescript-timers/interval.ts
+++ typescript-timers/interval.ts
@@ -1,12 +1,12 @@
 const $countdownDisplay = document.querySelector('.countdown-display');
 if (!$countdownDisplay) throw new Error('$countdownDisplay query failed');
 
-let count = 3;
+let count = 4;
 
 const intervalId = setInterval(() => {
-  $countdownDisplay.textContent = `${count}`;
   count--;
-  if (count === 0) {
+  $countdownDisplay.textContent = `${count}`;
+  if (count < 1) {
     clearInterval(intervalId);
     $countdownDisplay.textContent = `~Earth Beeeelooowww Us~`;
   }
