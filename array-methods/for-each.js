eeed01470c6fe9a48399e88022b13906a6a39973
Mike Glenn <mjglenn+lf@gmail.com>
2024-11-19 23:51:28 +0000

    Completed array-methods exercise.

---
+++ array-methods/for-each.js
@@ -0,0 +1,4 @@
+'use strict';
+const values = [10, 1, 22, 23, 41, 5, 18, 7, 80, 9];
+values.forEach((element) => console.log(element));
+values.forEach((item, index) => console.log(values[values.length - 1 - index]));
---
+++ array-methods/for-each.ts
@@ -0,0 +1,5 @@
+const values = [10, 1, 22, 23, 41, 5, 18, 7, 80, 9];
+
+values.forEach((element) => console.log(element));
+
+values.forEach((item, index) => console.log(values[values.length - 1 - index]));
