2963f152d71afda0bfef2483b6d8e46d0f8942c1
Mike Glenn <mjglenn+lf@gmail.com>
2024-12-09 23:57:00 +0000

    Completed react-components exercise.

---
+++ react-components/my-app/src/Description.tsx
@@ -0,0 +1,15 @@
+export function Description() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mollis
+      tempor justo ut pulvinar. Donec lectus metus, viverra in accumsan non,
+      commodo id est. Aliquam id facilisis mauris, in vehicula enim. Nulla
+      pulvinar vulputate hendrerit. Curabitur finibus efficitur sapien, vel
+      ultrices nisl luctus ut. Vivamus fermentum lobortis pharetra. Nulla
+      viverra consectetur ullamcorper. Duis sollicitudin dolor ut quam dapibus
+      dapibus. Donec quis risus at turpis accumsan fringilla sit amet id nibh.
+      Maecenas eget maximus risus. Aenean non tempor augue, non sollicitudin
+      tortor.
+    </p>
+  );
+}
