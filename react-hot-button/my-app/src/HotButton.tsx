9394772bf895a2badf3ad4d196f3e36dd2c844fb
Mike Glenn <mjglenn+lf@gmail.com>
2024-12-11 18:49:04 +0000

    moved state from App to HotButton

--- react-hot-button/my-app/src/HotButton.tsx
+++ react-hot-button/my-app/src/HotButton.tsx
@@ -1,16 +1,31 @@
-type Props = {
-  onHotButtonClick: () => void;
-  classBgColor: string;
-  clicks: number;
-};
+import { useState } from 'react';
 
-export function HotButton({ onHotButtonClick, classBgColor, clicks }: Props) {
+export function HotButton() {
+  const [count, setCount] = useState(0);
+
+  let bgColor = '';
+  if (count < 3) {
+    bgColor = '';
+  } else if (count < 6) {
+    bgColor = 'hot-button0-coldest';
+  } else if (count < 9) {
+    bgColor = 'hot-button1-colder';
+  } else if (count < 12) {
+    bgColor = 'hot-button2-cold';
+  } else if (count < 15) {
+    bgColor = 'hot-button3-hot';
+  } else if (count < 18) {
+    bgColor = 'hot-button4-hotter';
+  } else {
+    bgColor = 'hot-button5-hottest';
+  }
+
   return (
     <>
-      <button onClick={onHotButtonClick} className={classBgColor}>
+      <button onClick={() => setCount(count + 1)} className={bgColor}>
         Hot Button
       </button>
-      <div>{clicks} Clicks</div>
+      <div>{count} Clicks</div>
     </>
   );
 }
