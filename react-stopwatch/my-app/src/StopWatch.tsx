77d7e461a6e370cf4e4040e4c98beac1ac932df2
Mike Glenn <mjglenn+lf@gmail.com>
2024-12-13 18:02:06 +0000

    Completed react-stopwatch exercise.

--- react-stopwatch/my-app/src/StopWatch.tsx
+++ react-stopwatch/my-app/src/StopWatch.tsx
@@ -2,24 +2,37 @@ import './StopWatch.css';
 import { useState } from 'react';
 import { FaPause, FaPlay } from 'react-icons/fa';
 
+let StopWatchIcon = FaPlay;
+
 export function StopWatch() {
   const [intervalId, setIntervalId] = useState<NodeJS.Timeout>();
-  const [count, setCount] = useState(0);
-
-  let StopWatchIcon = FaPause;
-  StopWatchIcon = FaPlay;
-
-  function handleIconClick() {
-    setIntervalId(() => setInterval(() => setCount(count + 1), 1000));
-    clearInterval(intervalId);
-  }
+  const [elapsedSeconds, setElapsedSeconds] = useState(0);
 
   return (
     <>
-      <div className="stopwatch-wrapper">
-        <div className="stopwatch-face">0</div>
+      <div className="stopwatch-face">
+        <div className="stopwatch-seconds" onClick={() => setElapsedSeconds(0)}>
+          {elapsedSeconds}
+        </div>
       </div>
-      <StopWatchIcon className="stopwatch-icon" onClick={handleIconClick} />
+      <StopWatchIcon
+        className="stopwatch-icon"
+        onClick={() => {
+          if (intervalId === undefined) {
+            StopWatchIcon = FaPause;
+            setIntervalId(
+              setInterval(
+                () => setElapsedSeconds((elapsedSeconds) => elapsedSeconds + 1),
+                1000
+              )
+            );
+          } else {
+            StopWatchIcon = FaPlay;
+            clearInterval(intervalId);
+            setIntervalId(undefined);
+          }
+        }}
+      />
     </>
   );
 }
