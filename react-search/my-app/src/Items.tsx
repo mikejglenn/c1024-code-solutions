7d943ba15449d1bb06f33552b2ef9a89a79fdd26
Mike Glenn <mjglenn+lf@gmail.com>
2024-12-16 20:05:10 +0000

    items conditional to ternary operator

--- react-search/my-app/src/Items.tsx
+++ react-search/my-app/src/Items.tsx
@@ -3,12 +3,9 @@ type Props = {
 };
 
 export function Items({ filteredItems }: Props) {
-  let items;
-  if (filteredItems.length > 0) {
-    items = filteredItems.map((item, i) => <div key={i}>{item}</div>);
-  } else {
-    items = <div>No items match the filter.</div>;
-  }
-
-  return items;
+  return filteredItems.length > 0 ? (
+    filteredItems.map((item, i) => <div key={i}>{item}</div>)
+  ) : (
+    <div>No items match the filter.</div>
+  );
 }
