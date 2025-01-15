3a32ef3993708bd0ddb9940a6d2525c92040fa8c
Mike Glenn <mjglenn+lf@gmail.com>
2025-01-15 18:44:48 +0000

    Completed fetch-in-react exercise.

---
+++ fetch-in-react/fetch-in-react-quiz-notes.md
@@ -0,0 +1,51 @@
+# fetch-in-react-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What JavaScript function in the browser can be used in React to make HTTP requests to a server?
+  `fetch`
+
+- What two things need to be done to properly handle HTTP request errors? Why?
+  `async/await and error handling. might not get response. might get a response of an error.`
+
+- How can `useEffect` be used to load data for a component?
+  `async function inside useEffect calls data for component`
+
+- How do you use `useEffect` to load component data just once when the component mounts?
+  `empty dependency array`
+
+- How do you use `useEffect` to load component data every time the data key changes?
+  `have a dependency in the dependency array`
+
+- In a large-scale production app, what are some better alternatives for loading and managing backend data?
+  `React Query and Vercel SWR`
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
---
+++ fetch-in-react/my-app/src/UserDetails.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import { UserCard } from './UserCard';
+import type { User } from './Users';
+
+type Props = {
+  userId: number;
+  onCancel: () => void;
+};
+export function UserDetails({ userId, onCancel }: Props) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<unknown>();
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    async function getUser() {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/users/${userId}`
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const userData = (await response.json()) as User;
+        setUser(userData);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    setIsLoading(true);
+    getUser();
+  }, [userId]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error || !user) {
+    console.error('Fetch error:', error);
+    return (
+      <p>Error! {error instanceof Error ? error.message : 'Unknown Error'}</p>
+    );
+  }
+
+  return (
+    <div>
+      <button onClick={() => onCancel()}>Close</button>
+      <UserCard user={user} />
+    </div>
+  );
+}
