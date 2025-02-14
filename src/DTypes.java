89d8cf881155aac4f18460010c46520a751ea4fc
Mike Glenn <mjglenn+lf@gmail.com>
2025-02-13 16:15:00 -0800

    got modules to work

--- src/DTypes.java
+++ src/DTypes.java
@@ -4,13 +4,13 @@ public class DTypes {
     int x = 24;
     String s = "Hello, World!";
     double p = 3.14159;
-    int[] f = {1, 1, 2, 3, 5, 8};
+    // int[] f = {1, 1, 2, 3, 5, 8};
     var count = 24;
     // var list = new ArrayList<String>();
     System.out.println(x);
     System.out.println(s);
     System.out.println(p);
-    System.out.println(f);
+    // System.out.println(f.toString());
     System.out.println(count);
     // System.out.println(list);
   }
