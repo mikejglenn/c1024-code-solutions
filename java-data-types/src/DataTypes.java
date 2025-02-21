462fd4db05f3232092a182382a3053b99be9d536
Mike Glenn <mjglenn+lf@gmail.com>
2025-02-21 11:58:08 -0800

    Completed java-data-types exercise.

---
+++ java-data-types/src/DataTypes.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DataTypes {
+  public static void main(String[] args) {
+  int x = 24;
+  String s = "Hello, World!";
+  double p = 3.14159;
+  int[] f = {1, 1, 2, 3, 5, 8};
+  var count = 24;
+  var list = new ArrayList<String>();
+  System.out.println(x);
+  System.out.println(s);
+  System.out.println(p);
+  System.out.println(Arrays.toString(f));
+  System.out.println(count);
+  System.out.println(list);
+  }
+}
